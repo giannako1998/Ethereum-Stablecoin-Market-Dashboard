94ccb70dde42d04d243edd4e59aff3c15be67367
giannako1998 <g.giannako1998@gmail.com>
2026-01-19 12:50:49 +0200

    Delete USDT_Daily_Transfer_Volume(Ethereum).sql

--- USDT_Daily_Transfer_Volume(Ethereum).sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    date_trunc('day', evt_block_time) AS time,
-    SUM(value / 1e6) AS transfer_volume
-FROM erc20_ethereum.evt_Transfer
-WHERE contract_address = 0xdAC17F958D2ee523a2206206994597C13D831ec7
-AND evt_block_time > now() - interval '30' day
-GROUP BY 1
-ORDER BY 1 DESC
